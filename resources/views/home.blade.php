e99c8ba4ac8bbb267c829f884946a22238ce465e
luonverak <112792376+luonverak@users.noreply.github.com>
2024-02-27 22:54:24 +0700

    code commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="{{url('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css')}}" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
     <h1>Student Information</h1>
@@ -17,7 +18,7 @@
         </select> <br> <br>
         <input type="text" name="age" > <br> <br>
         <input type="file" name="profile" > <br> <br>
-        <input type="submit" name="" value="Submit Data">
+        <input class="btn btn-primary" type="submit" name="" value="Submit Data">
     </form>
 </body>
 </html>
